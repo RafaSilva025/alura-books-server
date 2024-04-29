5ed9f42ce88590352758fed3e28db3a5fb8439df
Rafa <rafasilvasimoes25@gmail.com>
2024-04-29 11:36:19 -0300

    atualizando codigo

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,16 +1,20 @@
 const { Router } = require("express");
-const { getLivros } = require("../controllers/livros");
+const { getLivros, getLivro } = require("../controllers/livros");
 
 const router = Router()
 
-
 router.get("/", getLivros)
+
+router.get("/:id", getLivro)
+
 router.post("/", (req, res) => {
     res.send("você fez um POST livros")
 })
+
 router.patch("/", (req, res) => {
     res.send("você fez um POST livros")
 })
+
 router.delete("/", (req, res) => {
     res.send("você fez um POST livros")
 })
--- servicos/livro.js
+++ servicos/livro.js
@@ -4,6 +4,13 @@ function getTodosLivros() {
     JSON.parse( fs.readFileSync("livros.json"))
 }
 
+function getLivrosPorID(id) {
+    const livros = JSON.parse(fs.readFileSync("livros.json"))
+
+    const livroFiltrado = livros.filter( livro => livro.id === id) [0]
+}
+
 module.exports = {
-    getTodosLivros
+    getTodosLivros, 
+    getLivrosPorID
 }
